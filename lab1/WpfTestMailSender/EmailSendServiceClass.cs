f9863908512d010902a66f8cb5659b99ca186a80
logovol <pasha1985@inbox.ru>
2020-09-27 12:57:00 +0300

    homework-1 done

---
+++ lab1/WpfTestMailSender/EmailSendServiceClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+
+namespace WpfTestMailSender
+{
+    public class EmailSendServiceClass
+    {
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        
+        public SecureString SecurePassword { get; set; }
+
+        public EmailSendServiceClass(string _subject, string _body, SecureString _password)
+        {
+            Subject        = _subject;
+            Body           = _body;
+            SecurePassword = _password;
+        }
+
+        public string Send()
+        {
+            Settings st = new Settings();
+            List<MailAddress> listStrMails = st.To;// Список email'ов //кому мы отправляем письмо
+            foreach (var mail in listStrMails)
+            {
+                // Используем using, чтобы гарантированно удалить объект MailMessage после использования                
+                using (MailMessage mm = new MailMessage(st.From, mail))
+                {
+                    // Формируем письмо
+                    mm.Subject = Subject; // Заголовок письма
+                    mm.Body = Body;       // Тело письма
+                    mm.IsBodyHtml = false;           // Не используем html в теле письма
+                                                     // Авторизуемся на smtp-сервере и отправляем письмо
+                                                     // Оператор using гарантирует вызов метода Dispose, даже если при вызове 
+                                                     // методов в объекте происходит исключение.
+                    using (SmtpClient sc = new SmtpClient(st.SmtpAddress, st.SmtpPort))
+                    {
+                        sc.EnableSsl = true;
+                        sc.Credentials = new NetworkCredential(st.From.Address, SecurePassword);
+                        try
+                        {
+                            sc.Send(mm);
+                        }
+                        catch (Exception ex)
+                        {
+                            return $"Невозможно отправить письмо {ex.ToString()}";
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ lab1/WpfTestMailSender/SendErrorWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfTestMailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для SendErrorWindow.xaml
+    /// </summary>
+    public partial class SendErrorWindow : Window
+    {
+        public SendErrorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
